cd849c9d4f95a7a7342fb63364c61ee6726d33ae
phard <pharder123@gmail.com>
2021-06-01 09:35:08 -0400

    fake data for today

--- Content/components/stats/Chart.jsx
+++ Content/components/stats/Chart.jsx
@@ -2,32 +2,36 @@
 
 const data = [
     {
+        name: '5/25/21',
+        TestDrink1: 7.2,
+    },
+    {
+        name: '5/26/21',
+        TestDrink1: 1.2,
+    },
+    {
+        name: '5/27/21',
+        TestDrink1: 3.1,
+    },
+    {
+        name: '6/28/21',
+        TestDrink1: 3.0,
+    },
+    {
+        name: '6/29/21',
+        TestDrink1: 5.4,
+    },
+    {
         name: '5/30/21',
-        TestDrink1: 12,
+        TestDrink1: 2.7,
     },
     {
         name: '5/31/21',
-        TestDrink1: 31,
-    },
-    {
-        name: '6/1/21',
-        TestDrink1: 3,
-    },
-    {
-        name: '6/2/21',
-        TestDrink1: 5,
-    },
-    {
-        name: '6/3/21',
-        TestDrink1: 7,
-    },
-    {
-        name: '6/4/21',
         TestDrink1: 0,
     },
     {
-        name: '6/5/21',
-        TestDrink1: 2,
+        name: '6/1/21',
+        TestDrink1: 5.2,
     },
 ];
 
